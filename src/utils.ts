952138742aa158e086dccbb5a0460cde3b2b3f8b
Xmader <xmader@outlook.com>
2019-11-11 21:44:47 -0500

    更新注释

--- src/utils.ts
+++ src/utils.ts
@@ -2,6 +2,8 @@
 export const getIndexPath = (id: number) => {
     const idStr = String(id)
     // 获取最后三位，倒序排列
+    // x, y, z are the reversed last digits of the score id. Example: id 123456789, x = 9, y = 8, z = 7
+    // https://developers.musescore.com/#/file-urls
     // "5449062" -> ["2", "6", "0"]
     const indexN = idStr.split("").reverse().slice(0, 3)
     return indexN.join("/")
