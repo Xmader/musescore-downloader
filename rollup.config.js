83ef26ead14ff7164e3cc66efc4c497d797717c6
Xmader <xmader@outlook.com>
2019-12-01 16:25:13 -0500

    include worker

--- rollup.config.js
+++ rollup.config.js
@@ -5,6 +5,7 @@ import builtins from "rollup-plugin-node-builtins"
 import nodeGlobals from "rollup-plugin-node-globals"
 import json from "@rollup/plugin-json"
 import fs from "fs"
+import multiEntry from "rollup-plugin-multi-entry"
 
 const getBannerText = () => {
     const packageJson = JSON.parse(fs.readFileSync("./package.json", "utf-8"))
@@ -14,54 +15,74 @@ const getBannerText = () => {
     return bannerText
 }
 
-export default {
-    input: "src/main.ts",
-    output: {
-        file: "dist/main.js",
-        format: "iife",
-        sourcemap: false,
-        banner: getBannerText,
-        // intro: "const global = typeof window === 'object' && window.window === window ? window : typeof self === 'object' && self.self === self ? self : typeof commonjsGlobal === 'object' && commonjsGlobal.global === commonjsGlobal ? commonjsGlobal : void 0"
-    },
-    plugins: [
-        typescript({
-            target: "ES6",
-            sourceMap: false,
-            allowJs: true,
-            lib: [
-                "ES6",
-                "dom"
-            ],
-        }),
-        resolve({
-            preferBuiltins: true,
-            jsnext: true,
-            extensions: [".js", ".ts"]
-        }),
-        commonjs({
-            extensions: [".js", ".ts"]
-        }),
-        json(),
-        builtins(),
-        nodeGlobals({
-            dirname: false,
-            filename: false,
-            baseDir: false,
-        }),
-        {
-            /**
-             * remove tslib license comments
-             * @param {string} code 
-             * @param {string} id 
-             */
-            transform(code, id) {
-                if (id.includes("tslib")) {
-                    code = code.split(/\r?\n/g).slice(15).join("\n")
-                }
-                return {
-                    code
-                }
+const plugins = [
+    typescript({
+        target: "ES6",
+        sourceMap: false,
+        allowJs: true,
+        lib: [
+            "ES6",
+            "dom"
+        ],
+    }),
+    resolve({
+        preferBuiltins: true,
+        jsnext: true,
+        extensions: [".js", ".ts"]
+    }),
+    commonjs({
+        extensions: [".js", ".ts"]
+    }),
+    json(),
+    builtins(),
+    nodeGlobals({
+        dirname: false,
+        filename: false,
+        baseDir: false,
+    }),
+    {
+        /**
+         * remove tslib license comments
+         * @param {string} code 
+         * @param {string} id 
+         */
+        transform(code, id) {
+            if (id.includes("tslib")) {
+                code = code.split(/\r?\n/g).slice(15).join("\n")
             }
+            return {
+                code
+            }
+        }
+    },
+]
+
+export default [
+    {
+        input: "src/worker.ts",
+        output: {
+            file: "dist/cache/worker.js",
+            format: "iife",
+            name: "PDFWorker",
+            sourcemap: false,
         },
-    ]
-}
+        plugins,
+    },
+    {
+        input: [
+            "dist/cache/worker.js",
+            "src/main.ts",
+        ],
+        output: {
+            file: "dist/main.js",
+            format: "iife",
+            sourcemap: false,
+            banner: getBannerText,
+            // intro: "const global = typeof window === 'object' && window.window === window ? window : typeof self === 'object' && self.self === self ? self : typeof commonjsGlobal === 'object' && commonjsGlobal.global === commonjsGlobal ? commonjsGlobal : void 0"
+        },
+        plugins:[
+            ...plugins,
+            multiEntry({ exports: false }),
+        ],
+    },
+]
