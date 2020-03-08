7a36eacee278491c5fbfdd735aac7d7e3a076585
Xmader <xmader@outlook.com>
2020-03-08 17:23:22 -0400

    v0.5.3

--- dist/main.js
+++ dist/main.js
@@ -3,7 +3,7 @@
 // @namespace    https://www.xmader.com/
 // @homepageURL  https://github.com/Xmader/musescore-downloader/
 // @supportURL   https://github.com/Xmader/musescore-downloader/issues
-// @version      0.5.2
+// @version      0.5.3
 // @description  download sheet music from musescore.com for free, no login or Musescore Pro required | 免登录、免 Musescore Pro，免费下载 musescore.com 上的曲谱
 // @author       Xmader
 // @match        https://musescore.com/*/*
