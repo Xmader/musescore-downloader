14eb0e8f306cd01386582f90f8ddaab45fdbe59b
Xmader <xmader@outlook.com>
2020-10-18 07:22:11 -0400

    v0.8.3 - #32

--- dist/main.js
+++ dist/main.js
@@ -3,7 +3,7 @@
 // @namespace    https://www.xmader.com/
 // @homepageURL  https://github.com/Xmader/musescore-downloader/
 // @supportURL   https://github.com/Xmader/musescore-downloader/issues
-// @version      0.8.2
+// @version      0.8.3
 // @description  download sheet music from musescore.com for free, no login or Musescore Pro required | 免登录、免 Musescore Pro，免费下载 musescore.com 上的曲谱
 // @author       Xmader
 // @match        https://musescore.com/*/*
@@ -26339,16 +26339,8 @@ Please pipe the document into a Node stream.\
         get baseUrl() {
             const thumbnailUrl = this.playerdata.thumbnails.original;
             const { origin, pathname } = new URL(thumbnailUrl);
+            // remove the last part
             return origin + pathname.split('/').slice(0, -1).join('/') + '/';
-        },
-        get mxlUrl() {
-            return this.baseUrl + 'score.mxl';
-        },
-        get midiUrl() {
-            return this.baseUrl + 'score.mid';
-        },
-        get mp3Url() {
-            return this.baseUrl + 'score.mp3';
         },
         get msczUrl() {
             // https://github.com/Xmader/cloudflare-worker-musescore-mscz
@@ -26369,6 +26361,346 @@ Please pipe the document into a Node stream.\
         },
     };
 
+    var crypt = createCommonjsModule(function (module) {
+    (function() {
+      var base64map
+          = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/',
+
+      crypt = {
+        // Bit-wise rotation left
+        rotl: function(n, b) {
+          return (n << b) | (n >>> (32 - b));
+        },
+
+        // Bit-wise rotation right
+        rotr: function(n, b) {
+          return (n << (32 - b)) | (n >>> b);
+        },
+
+        // Swap big-endian to little-endian and vice versa
+        endian: function(n) {
+          // If number given, swap endian
+          if (n.constructor == Number) {
+            return crypt.rotl(n, 8) & 0x00FF00FF | crypt.rotl(n, 24) & 0xFF00FF00;
+          }
+
+          // Else, assume array and swap all items
+          for (var i = 0; i < n.length; i++)
+            n[i] = crypt.endian(n[i]);
+          return n;
+        },
+
+        // Generate an array of any length of random bytes
+        randomBytes: function(n) {
+          for (var bytes = []; n > 0; n--)
+            bytes.push(Math.floor(Math.random() * 256));
+          return bytes;
+        },
+
+        // Convert a byte array to big-endian 32-bit words
+        bytesToWords: function(bytes) {
+          for (var words = [], i = 0, b = 0; i < bytes.length; i++, b += 8)
+            words[b >>> 5] |= bytes[i] << (24 - b % 32);
+          return words;
+        },
+
+        // Convert big-endian 32-bit words to a byte array
+        wordsToBytes: function(words) {
+          for (var bytes = [], b = 0; b < words.length * 32; b += 8)
+            bytes.push((words[b >>> 5] >>> (24 - b % 32)) & 0xFF);
+          return bytes;
+        },
+
+        // Convert a byte array to a hex string
+        bytesToHex: function(bytes) {
+          for (var hex = [], i = 0; i < bytes.length; i++) {
+            hex.push((bytes[i] >>> 4).toString(16));
+            hex.push((bytes[i] & 0xF).toString(16));
+          }
+          return hex.join('');
+        },
+
+        // Convert a hex string to a byte array
+        hexToBytes: function(hex) {
+          for (var bytes = [], c = 0; c < hex.length; c += 2)
+            bytes.push(parseInt(hex.substr(c, 2), 16));
+          return bytes;
+        },
+
+        // Convert a byte array to a base-64 string
+        bytesToBase64: function(bytes) {
+          for (var base64 = [], i = 0; i < bytes.length; i += 3) {
+            var triplet = (bytes[i] << 16) | (bytes[i + 1] << 8) | bytes[i + 2];
+            for (var j = 0; j < 4; j++)
+              if (i * 8 + j * 6 <= bytes.length * 8)
+                base64.push(base64map.charAt((triplet >>> 6 * (3 - j)) & 0x3F));
+              else
+                base64.push('=');
+          }
+          return base64.join('');
+        },
+
+        // Convert a base-64 string to a byte array
+        base64ToBytes: function(base64) {
+          // Remove non-base-64 characters
+          base64 = base64.replace(/[^A-Z0-9+\/]/ig, '');
+
+          for (var bytes = [], i = 0, imod4 = 0; i < base64.length;
+              imod4 = ++i % 4) {
+            if (imod4 == 0) continue;
+            bytes.push(((base64map.indexOf(base64.charAt(i - 1))
+                & (Math.pow(2, -2 * imod4 + 8) - 1)) << (imod4 * 2))
+                | (base64map.indexOf(base64.charAt(i)) >>> (6 - imod4 * 2)));
+          }
+          return bytes;
+        }
+      };
+
+      module.exports = crypt;
+    })();
+    });
+
+    var charenc = {
+      // UTF-8 encoding
+      utf8: {
+        // Convert a string to a byte array
+        stringToBytes: function(str) {
+          return charenc.bin.stringToBytes(unescape(encodeURIComponent(str)));
+        },
+
+        // Convert a byte array to a string
+        bytesToString: function(bytes) {
+          return decodeURIComponent(escape(charenc.bin.bytesToString(bytes)));
+        }
+      },
+
+      // Binary encoding
+      bin: {
+        // Convert a string to a byte array
+        stringToBytes: function(str) {
+          for (var bytes = [], i = 0; i < str.length; i++)
+            bytes.push(str.charCodeAt(i) & 0xFF);
+          return bytes;
+        },
+
+        // Convert a byte array to a string
+        bytesToString: function(bytes) {
+          for (var str = [], i = 0; i < bytes.length; i++)
+            str.push(String.fromCharCode(bytes[i]));
+          return str.join('');
+        }
+      }
+    };
+
+    var charenc_1 = charenc;
+
+    /*!
+     * Determine if an object is a Buffer
+     *
+     * @author   Feross Aboukhadijeh <https://feross.org>
+     * @license  MIT
+     */
+
+    // The _isBuffer check is for Safari 5-7 support, because it's missing
+    // Object.prototype.constructor. Remove this eventually
+    var isBuffer_1 = function (obj) {
+      return obj != null && (isBuffer(obj) || isSlowBuffer(obj) || !!obj._isBuffer)
+    };
+
+    function isBuffer (obj) {
+      return !!obj.constructor && typeof obj.constructor.isBuffer === 'function' && obj.constructor.isBuffer(obj)
+    }
+
+    // For Node v0.10 support. Remove this eventually.
+    function isSlowBuffer (obj) {
+      return typeof obj.readFloatLE === 'function' && typeof obj.slice === 'function' && isBuffer(obj.slice(0, 0))
+    }
+
+    var md5 = createCommonjsModule(function (module) {
+    (function(){
+      var crypt$1 = crypt,
+          utf8 = charenc_1.utf8,
+          isBuffer = isBuffer_1,
+          bin = charenc_1.bin,
+
+      // The core
+      md5 = function (message, options) {
+        // Convert to byte array
+        if (message.constructor == String)
+          if (options && options.encoding === 'binary')
+            message = bin.stringToBytes(message);
+          else
+            message = utf8.stringToBytes(message);
+        else if (isBuffer(message))
+          message = Array.prototype.slice.call(message, 0);
+        else if (!Array.isArray(message) && message.constructor !== Uint8Array)
+          message = message.toString();
+        // else, assume byte array already
+
+        var m = crypt$1.bytesToWords(message),
+            l = message.length * 8,
+            a =  1732584193,
+            b = -271733879,
+            c = -1732584194,
+            d =  271733878;
+
+        // Swap endian
+        for (var i = 0; i < m.length; i++) {
+          m[i] = ((m[i] <<  8) | (m[i] >>> 24)) & 0x00FF00FF |
+                 ((m[i] << 24) | (m[i] >>>  8)) & 0xFF00FF00;
+        }
+
+        // Padding
+        m[l >>> 5] |= 0x80 << (l % 32);
+        m[(((l + 64) >>> 9) << 4) + 14] = l;
+
+        // Method shortcuts
+        var FF = md5._ff,
+            GG = md5._gg,
+            HH = md5._hh,
+            II = md5._ii;
+
+        for (var i = 0; i < m.length; i += 16) {
+
+          var aa = a,
+              bb = b,
+              cc = c,
+              dd = d;
+
+          a = FF(a, b, c, d, m[i+ 0],  7, -680876936);
+          d = FF(d, a, b, c, m[i+ 1], 12, -389564586);
+          c = FF(c, d, a, b, m[i+ 2], 17,  606105819);
+          b = FF(b, c, d, a, m[i+ 3], 22, -1044525330);
+          a = FF(a, b, c, d, m[i+ 4],  7, -176418897);
+          d = FF(d, a, b, c, m[i+ 5], 12,  1200080426);
+          c = FF(c, d, a, b, m[i+ 6], 17, -1473231341);
+          b = FF(b, c, d, a, m[i+ 7], 22, -45705983);
+          a = FF(a, b, c, d, m[i+ 8],  7,  1770035416);
+          d = FF(d, a, b, c, m[i+ 9], 12, -1958414417);
+          c = FF(c, d, a, b, m[i+10], 17, -42063);
+          b = FF(b, c, d, a, m[i+11], 22, -1990404162);
+          a = FF(a, b, c, d, m[i+12],  7,  1804603682);
+          d = FF(d, a, b, c, m[i+13], 12, -40341101);
+          c = FF(c, d, a, b, m[i+14], 17, -1502002290);
+          b = FF(b, c, d, a, m[i+15], 22,  1236535329);
+
+          a = GG(a, b, c, d, m[i+ 1],  5, -165796510);
+          d = GG(d, a, b, c, m[i+ 6],  9, -1069501632);
+          c = GG(c, d, a, b, m[i+11], 14,  643717713);
+          b = GG(b, c, d, a, m[i+ 0], 20, -373897302);
+          a = GG(a, b, c, d, m[i+ 5],  5, -701558691);
+          d = GG(d, a, b, c, m[i+10],  9,  38016083);
+          c = GG(c, d, a, b, m[i+15], 14, -660478335);
+          b = GG(b, c, d, a, m[i+ 4], 20, -405537848);
+          a = GG(a, b, c, d, m[i+ 9],  5,  568446438);
+          d = GG(d, a, b, c, m[i+14],  9, -1019803690);
+          c = GG(c, d, a, b, m[i+ 3], 14, -187363961);
+          b = GG(b, c, d, a, m[i+ 8], 20,  1163531501);
+          a = GG(a, b, c, d, m[i+13],  5, -1444681467);
+          d = GG(d, a, b, c, m[i+ 2],  9, -51403784);
+          c = GG(c, d, a, b, m[i+ 7], 14,  1735328473);
+          b = GG(b, c, d, a, m[i+12], 20, -1926607734);
+
+          a = HH(a, b, c, d, m[i+ 5],  4, -378558);
+          d = HH(d, a, b, c, m[i+ 8], 11, -2022574463);
+          c = HH(c, d, a, b, m[i+11], 16,  1839030562);
+          b = HH(b, c, d, a, m[i+14], 23, -35309556);
+          a = HH(a, b, c, d, m[i+ 1],  4, -1530992060);
+          d = HH(d, a, b, c, m[i+ 4], 11,  1272893353);
+          c = HH(c, d, a, b, m[i+ 7], 16, -155497632);
+          b = HH(b, c, d, a, m[i+10], 23, -1094730640);
+          a = HH(a, b, c, d, m[i+13],  4,  681279174);
+          d = HH(d, a, b, c, m[i+ 0], 11, -358537222);
+          c = HH(c, d, a, b, m[i+ 3], 16, -722521979);
+          b = HH(b, c, d, a, m[i+ 6], 23,  76029189);
+          a = HH(a, b, c, d, m[i+ 9],  4, -640364487);
+          d = HH(d, a, b, c, m[i+12], 11, -421815835);
+          c = HH(c, d, a, b, m[i+15], 16,  530742520);
+          b = HH(b, c, d, a, m[i+ 2], 23, -995338651);
+
+          a = II(a, b, c, d, m[i+ 0],  6, -198630844);
+          d = II(d, a, b, c, m[i+ 7], 10,  1126891415);
+          c = II(c, d, a, b, m[i+14], 15, -1416354905);
+          b = II(b, c, d, a, m[i+ 5], 21, -57434055);
+          a = II(a, b, c, d, m[i+12],  6,  1700485571);
+          d = II(d, a, b, c, m[i+ 3], 10, -1894986606);
+          c = II(c, d, a, b, m[i+10], 15, -1051523);
+          b = II(b, c, d, a, m[i+ 1], 21, -2054922799);
+          a = II(a, b, c, d, m[i+ 8],  6,  1873313359);
+          d = II(d, a, b, c, m[i+15], 10, -30611744);
+          c = II(c, d, a, b, m[i+ 6], 15, -1560198380);
+          b = II(b, c, d, a, m[i+13], 21,  1309151649);
+          a = II(a, b, c, d, m[i+ 4],  6, -145523070);
+          d = II(d, a, b, c, m[i+11], 10, -1120210379);
+          c = II(c, d, a, b, m[i+ 2], 15,  718787259);
+          b = II(b, c, d, a, m[i+ 9], 21, -343485551);
+
+          a = (a + aa) >>> 0;
+          b = (b + bb) >>> 0;
+          c = (c + cc) >>> 0;
+          d = (d + dd) >>> 0;
+        }
+
+        return crypt$1.endian([a, b, c, d]);
+      };
+
+      // Auxiliary functions
+      md5._ff  = function (a, b, c, d, x, s, t) {
+        var n = a + (b & c | ~b & d) + (x >>> 0) + t;
+        return ((n << s) | (n >>> (32 - s))) + b;
+      };
+      md5._gg  = function (a, b, c, d, x, s, t) {
+        var n = a + (b & d | c & ~d) + (x >>> 0) + t;
+        return ((n << s) | (n >>> (32 - s))) + b;
+      };
+      md5._hh  = function (a, b, c, d, x, s, t) {
+        var n = a + (b ^ c ^ d) + (x >>> 0) + t;
+        return ((n << s) | (n >>> (32 - s))) + b;
+      };
+      md5._ii  = function (a, b, c, d, x, s, t) {
+        var n = a + (c ^ (b | ~d)) + (x >>> 0) + t;
+        return ((n << s) | (n >>> (32 - s))) + b;
+      };
+
+      // Package private blocksize
+      md5._blocksize = 16;
+      md5._digestsize = 16;
+
+      module.exports = function (message, options) {
+        if (message === undefined || message === null)
+          throw new Error('Illegal argument ' + message);
+
+        var digestbytes = crypt$1.wordsToBytes(md5(message, options));
+        return options && options.asBytes ? digestbytes :
+            options && options.asString ? bin.bytesToString(digestbytes) :
+            crypt$1.bytesToHex(digestbytes);
+      };
+
+    })();
+    });
+
+    const MAGIC = String(4 * 11 * 1607 * 2767); // '195649036' // I don't know what this is
+    const AUTH_LEN = 4;
+    const getApiUrl = (type, index) => {
+        // proxy
+        return `https://musescore.now.sh/api/jmuse?id=${scoreinfo.id}&type=${type}&index=${index}`;
+    };
+    const getApiAuth = (type, index) => {
+        const str = String(scoreinfo.id) + type + String(index) + MAGIC;
+        return md5(str).slice(0, AUTH_LEN);
+    };
+    const getFileUrl = (type, index = 0) => __awaiter(void 0, void 0, void 0, function* () {
+        const url = getApiUrl(type, index);
+        const auth = getApiAuth(type, index);
+        const r = yield fetch(url, {
+            headers: {
+                Authorization: auth,
+            },
+        });
+        const { info } = yield r.json();
+        return info.url;
+    });
+
     let pdfBlob;
     const _downloadPDF = (imgURLs, imgType, name = '') => __awaiter(void 0, void 0, void 0, function* () {
         if (pdfBlob) {
@@ -26385,9 +26717,15 @@ Please pipe the document into a Node stream.\
     const downloadPDF = () => __awaiter(void 0, void 0, void 0, function* () {
         const imgType = scoreinfo.sheetImgType;
         const pageCount = scoreinfo.pageCount;
-        const sheetImgURLs = Array.from({ length: pageCount }).map((_, i) => {
-            return scoreinfo.baseUrl + `score_${i}.${imgType}`;
+        const rs = Array.from({ length: pageCount }).map((_, i) => {
+            if (i === 0) { // The url to the first page is static. We don't need to use API to obtain it.
+                return scoreinfo.baseUrl + `score_${i}.${imgType}`;
+            }
+            else { // obtain image urls using the API
+                return getFileUrl('img', i);
+            }
         });
+        const sheetImgURLs = yield Promise.all(rs);
         return _downloadPDF(sheetImgURLs, imgType, scoreinfo.fileName);
     });
 
@@ -26542,8 +26880,24 @@ Please pipe the document into a Node stream.\
     (function (BtnAction) {
         BtnAction.PROCESSING_TEXT = 'Processing…';
         BtnAction.ERROR_TEXT = '❌Download Failed!';
+        const normalizeUrlInput = (url) => {
+            if (typeof url === 'function')
+                return url();
+            else
+                return url;
+        };
         BtnAction.openUrl = (url) => {
-            return () => window.open(url);
+            return BtnAction.process(() => __awaiter(this, void 0, void 0, function* () {
+                window.open(yield normalizeUrlInput(url));
+            }));
+        };
+        BtnAction.download = (url, filename) => {
+            return BtnAction.process(() => __awaiter(this, void 0, void 0, function* () {
+                const _url = yield normalizeUrlInput(url);
+                const r = yield fetch(_url);
+                const blob = yield r.blob();
+                saveAs(blob, filename);
+            }));
         };
         BtnAction.mscoreWindow = (fn) => {
             return (btnName, btn, setText) => __awaiter(this, void 0, void 0, function* () {
@@ -26599,6 +26953,7 @@ Please pipe the document into a Node stream.\
         // eslint-disable-next-line @typescript-eslint/no-floating-promises
         init();
         const btnList = new BtnList(getDownloadBtn());
+        const filename = scoreinfo.fileName;
         btnList.add({
             name: 'Download MSCZ',
             action: BtnAction.process(downloadMscz),
@@ -26612,17 +26967,17 @@ Please pipe the document into a Node stream.\
             action: BtnAction.mscoreWindow((w, score) => __awaiter(void 0, void 0, void 0, function* () {
                 const mxl = yield score.saveMxl();
                 const data = new Blob([mxl]);
-                saveAs(data, `${scoreinfo.fileName}.mxl`);
+                saveAs(data, `${filename}.mxl`);
                 w.close();
             })),
         });
         btnList.add({
             name: 'Download MIDI',
-            action: BtnAction.openUrl(scoreinfo.midiUrl),
+            action: BtnAction.download(() => getFileUrl('midi'), `${filename}.mid`),
         });
         btnList.add({
             name: 'Download MP3',
-            action: BtnAction.openUrl(scoreinfo.mp3Url),
+            action: BtnAction.download(() => getFileUrl('mp3'), `${filename}.mp3`),
         });
         btnList.add({
             name: 'Individual Parts',
@@ -26661,7 +27016,6 @@ Please pipe the document into a Node stream.\
                     const id = checked.value;
                     const partName = checked.alt;
                     yield score.setExcerptId(+id);
-                    const filename = scoreinfo.fileName;
                     const data = new Blob([yield score.savePdf()]);
                     saveAs(data, `${filename} - ${partName}.pdf`);
                 });
