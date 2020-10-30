e7c299536f85d2f6a595ba8c35728c3651709976
Xmader <xmader@outlook.com>
2020-10-29 20:41:25 -0400

    userscript update url

--- src/meta.js
+++ src/meta.js
@@ -3,8 +3,8 @@
 // @namespace    https://www.xmader.com/
 // @homepageURL  https://github.com/Xmader/musescore-downloader/
 // @supportURL   https://github.com/Xmader/musescore-downloader/issues
-// @updateURL    https://msdl.librescore.org/dist/main.js
-// @downloadURL  https://msdl.librescore.org/dist/main.js
+// @updateURL    https://msdl.librescore.org/install.user.js
+// @downloadURL  https://msdl.librescore.org/install.user.js
 // @version      %VERSION%
 // @description  download sheet music from musescore.com for free, no login or Musescore Pro required | 免登录、免 Musescore Pro，免费下载 musescore.com 上的曲谱
 // @author       Xmader
