65c1e02d8be24c49c5fd0f68ffcecbec4fcd3481
Xmader <xmader@outlook.com>
2020-11-30 15:26:01 -0500

    fix: the loadstart event is not fired on <img> elements in Chrome

--- rollup.config.js
+++ rollup.config.js
@@ -83,7 +83,7 @@ export default [
             format: "iife",
             sourcemap: false,
             banner: getBannerText,
-            intro: "const d =document.createElement('img');d.style.display='none';d.src='data:';d.once=false;d.setAttribute('onloadstart','if(this.once)return;this.once=true;this.remove();(' + function () {",
+            intro: "const d =document.createElement('img');d.style.display='none';d.src='data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==';d.once=false;d.setAttribute('onload','if(this.once)return;this.once=true;this.remove();(' + function () {",
             outro: "}.toString() + ')()');document.body.prepend(d)"
         },
         plugins,
