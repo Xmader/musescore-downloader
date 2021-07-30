7d7a838ab811dec2c8af1065354ed48477c03a36
Xmader <xmader@outlook.com>
2021-07-30 00:52:27 -0400

    refactor: file api auth

---
+++ src/file-magics.ts
@@ -0,0 +1,63 @@
+
+import { hookNative } from './anti-detection'
+import type { FileType } from './file'
+
+const TYPE_REG = /type=(img|mp3|midi)/
+
+/**
+ * I know this is super hacky.
+ */
+const magicHookConstr = (() => {
+  const l = {}
+
+  try {
+    const p = Object.getPrototypeOf(document.body)
+    Object.setPrototypeOf(document.body, null)
+
+    hookNative(document.body, 'append', () => {
+      return function (...nodes: Node[]) {
+        p.append.call(this, ...nodes)
+
+        if (nodes[0].nodeName === 'IFRAME') {
+          const iframe = nodes[0] as HTMLIFrameElement
+          const w = iframe.contentWindow as Window
+
+          hookNative(w, 'fetch', () => {
+            return function (url, init) {
+              const token = init?.headers?.Authorization
+              if (typeof url === 'string' && token) {
+                const m = url.match(TYPE_REG)
+                console.debug(url, token, m)
+                if (m) {
+                  const type = m[1]
+                  // eslint-disable-next-line no-unused-expressions
+                  l[type]?.(token)
+                }
+              }
+              return fetch(url, init)
+            }
+          })
+        }
+      }
+    })
+
+    Object.setPrototypeOf(document.body, p)
+  } catch (err) {
+    console.error(err)
+  }
+
+  return async (type: FileType) => {
+    return new Promise<string>((resolve) => {
+      l[type] = (token) => {
+        resolve(token)
+        magics[type] = token
+      }
+    })
+  }
+})()
+
+export const magics: Record<FileType, Promise<string>> = {
+  img: magicHookConstr('img'),
+  midi: magicHookConstr('midi'),
+  mp3: magicHookConstr('mp3'),
+}
--- src/file.ts
+++ src/file.ts
@@ -1,70 +1,10 @@
 /* eslint-disable no-extend-native */
 /* eslint-disable @typescript-eslint/no-unsafe-return */
 
-import { hookNative } from './anti-detection'
-import { console, useTimeout } from './utils'
+import { console, useTimeout, getFetch } from './utils'
+import { magics } from './file-magics'
 
-type FileType = 'img' | 'mp3' | 'midi'
-
-const TYPE_REG = /type=(img|mp3|midi)/
-
-/**
- * I know this is super hacky.
- */
-const magicHookConstr = (() => {
-  const l = {}
-
-  try {
-    const p = Object.getPrototypeOf(document.body)
-    Object.setPrototypeOf(document.body, null)
-
-    hookNative(document.body, 'append', () => {
-      return function (...nodes: Node[]) {
-        p.append.call(this, ...nodes)
-
-        if (nodes[0].nodeName === 'IFRAME') {
-          const iframe = nodes[0] as HTMLIFrameElement
-          const w = iframe.contentWindow as Window
-
-          hookNative(w, 'fetch', () => {
-            return function (url, init) {
-              const token = init?.headers?.Authorization
-              if (typeof url === 'string' && token) {
-                const m = url.match(TYPE_REG)
-                console.debug(url, token, m)
-                if (m) {
-                  const type = m[1]
-                  // eslint-disable-next-line no-unused-expressions
-                  l[type]?.(token)
-                }
-              }
-              return fetch(url, init)
-            }
-          })
-        }
-      }
-    })
-
-    Object.setPrototypeOf(document.body, p)
-  } catch (err) {
-    console.error(err)
-  }
-
-  return async (type: FileType) => {
-    return new Promise<string>((resolve) => {
-      l[type] = (token) => {
-        resolve(token)
-        magics[type] = token
-      }
-    })
-  }
-})()
-
-const magics: Record<FileType, Promise<string>> = {
-  img: magicHookConstr('img'),
-  midi: magicHookConstr('midi'),
-  mp3: magicHookConstr('mp3'),
-}
+export type FileType = 'img' | 'mp3' | 'midi'
 
 const getApiUrl = (id: number, type: FileType, index: number): string => {
   return `/api/jmuse?id=${id}&type=${type}&index=${index}&v2=1`
